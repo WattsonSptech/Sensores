85051e0c1d984f1641821499516eccbe52f40c90
F3gcastro <gcastro@f3capital.com.br>
2025-04-15 21:54:16 -0300

    Início da refatoração

---
+++ simulacao_sensores/DatabaseHelper.py
@@ -0,0 +1,15 @@
+class DatabaseHelper:
+
+    
+
+    def conectar(self) -> None:
+        pass
+
+    def gravar_registros(self, registros: list[object]):
+        pass
+
+    def desconectar(self) -> None:
+        pass
+
+
+DatabaseHelper().gravar_registros([])
