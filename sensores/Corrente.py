03dc28438bb5ad85891a4a433a918f21e8404bb2
Caetano Resende Domingues <caetano.domingues@sptech.school>
2025-04-25 20:33:43 -0300

    feat: adicionar cenarios de regra de negócio ao sensor de corrente

--- sensores/Corrente.py
+++ sensores/Corrente.py
@@ -1,4 +1,5 @@
 import random
+from interfaces.EnumCenarios import EnumCenarios
 from interfaces.ISimuladorSensor import ISimuladorSensor
 
 class Corrente(ISimuladorSensor):
@@ -10,15 +11,25 @@ class Corrente(ISimuladorSensor):
         self.nome_sensor = "RCI-32"
         self.unidade = "ampére"
 
-    def __formula_sensor__(self):
-        primary_current = round(random.randint(16, self.__MAX_CURRENT__), 4)
+    def __formula_sensor__(self, cenario: EnumCenarios):
 
-        percentual = primary_current / self.__MAX_CURRENT__ * 100
-        if percentual > 5 and percentual <= 120:
+        percentual = 0
+        match cenario:
+            case cenario.NORMAL:
+                primary_current = round(random.randint(400, self.__MAX_CURRENT__), 4)
+                percentual = primary_current / self.__MAX_CURRENT__ * 100
+            case cenario.TERRIVEL:
+                primary_current = round(random.randint(700, 120), 4)
+                percentual = primary_current / self.__MAX_CURRENT__ * 100
+            case cenario.EXCEPCIONAL:
+                primary_current = round(random.randint(80, 400), 4)
+                percentual = primary_current / self.__MAX_CURRENT__ * 100
+
+        if percentual > 50 and percentual <= 100:
             secundary_current = 5
-        elif percentual >= 2.5 and percentual < 5:
+        elif percentual >= 87.5 and percentual <= 120:
             secundary_current = round(primary_current / self.__ENROLL_PROPORTION__, 2)
-        else:
+        elif percentual >= 10 and percentual <= 50:
             secundary_current = round(primary_current / self.__ENROLL_PROPORTION__, 2)
 
         return secundary_current
