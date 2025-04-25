80757de915141280f2548e73ff206f60ab4e474f
DanielleMunakata <danielle.munakata@sptech.school>
2025-04-25 20:05:39 -0300

    Mudança temp

--- sensores/Temperatura.py
+++ sensores/Temperatura.py
@@ -1,23 +1,34 @@
 from interfaces.ISimuladorSensor import ISimuladorSensor
+from interfaces.EnumCenarios import EnumCenarios
 from interfaces.Registro import Registro
 import numpy as np
 
 class Temperatura(ISimuladorSensor):
     
-    __VALOR_BASE__ = 70
+    __VALOR_BASE__ = 72.5 
 
     def __init__(self) -> None:
         self.nome_sensor = "IR MLX90614"
         self.unidade = "°C"
 
-    def __formula_sensor__(self):
-        variacao_moment = np.random.normal(0, 0.1)
+    def __formula_sensor__(self, cenario: EnumCenarios):
+        variacao_moment = np.random.normal(0, 1.5)
+
+        propabilidade_pico: float
+        match cenario:
+            case cenario.TERRIVEL:
+                propabilidade_pico = 0.1
+            case cenario.EXCEPCIONAL:
+                propabilidade_pico = 0.000001
+            case _:
+                propabilidade_pico = 0.001
 
         pico = 0
-        if np.random.rand() < 0.001:
-            pico = 100 - self.__VALOR_BASE__ - variacao_moment  
+        if np.random.rand() < propabilidade_pico:
+            pico = max(0, np.random.pareto(2) * 10)
+            if self.__VALOR_BASE__ + variacao_moment + pico > 100:
+                pico = 100 - self.__VALOR_BASE__ - variacao_moment
 
         temperatura = self.__VALOR_BASE__ + variacao_moment + pico
 
         return Registro(self.nome_sensor, self.unidade, temperatura)
-
