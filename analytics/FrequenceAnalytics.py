09b03cc89a56ad92942a41b54ec7898f55765f32
edu-correa <eduardo.correa@sptech.school>
2025-05-06 20:38:51 -0300

    feat: adding analytics frequence

--- analytics/FrequenceAnalytics.py
+++ analytics/FrequenceAnalytics.py
@@ -2,7 +2,30 @@ import boto3
 import pandas as pd
 import pyspark
 from pyspark.sql import SparkSession
+import matplotlib.pyplot as plt
+import json
 
+
+def gerar_grafico():
+    dados_x = []
+    dados_y = []
+    with open('dados.json') as f:
+        for line in f:
+            item = json.loads(line)
+            if(item['valueType'] == 'Hz'):
+                dados_x.append(item['EventProcessedUtcTime'])
+                dados_y.append(item['value'])
+    
+    plt.plot(dados_x, dados_y, color='blue', linewidth=1)
+    plt.title("Gráfico Harmônicas")
+    plt.xlabel("Variação de Tempo")
+    plt.ylabel("Valor (%)")
+    plt.axhline(1, color='green', linestyle='-', linewidth=2, label='Estado Excelente')
+    plt.axhline(5, color='yellow', linestyle='-', linewidth=2, label='Estado Normal')
+    plt.axhline(100, color='red', linestyle='-', linewidth=2, label='Estado Terrível')
+    plt.axhline((sum(dados_y)/len(dados_y)), color='purple', linestyle='-', linewidth=2, label='Média')
+    plt.legend()
+    plt.show()
 
 spark = SparkSession.builder \
     .appName("Frequence Spark") \
@@ -21,6 +44,7 @@ df.show()
 df.show(1, vertical=True)
 
 df.select("frequency").describe().show()
+gerar_grafico()
 
 
 # s3 = boto3.resource('s3')
