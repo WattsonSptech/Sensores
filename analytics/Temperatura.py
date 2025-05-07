27649cdd404d5190ab8f7157b9e35f9b5a5404eb
DanielleMunakata <danielle.munakata@sptech.school>
2025-05-06 21:02:47 -0300

    feat: adding plots to temperature

--- analytics/Temperatura.py
+++ analytics/Temperatura.py
@@ -7,7 +7,7 @@ def gerar_grafico_temperatura():
     with open('dados.json') as f:
         for line in f:
             item = json.loads(line)
-            if item['valueType'] == 'Temperatura':
+            if item['valueType'] == 'Â°C' or item['valueType'] == '°C':
                 dados_x.append(item['EventProcessedUtcTime'])
                 dados_y.append(item['value'])
 
@@ -15,21 +15,18 @@ def gerar_grafico_temperatura():
     plt.title("Gráfico de Temperatura")
     plt.xlabel("Tempo")
     plt.ylabel("Temperatura (°C)")
-    
-    plt.axhline(60, color='green', linestyle='-', linewidth=2, label='Frio (Normal)')
-    plt.axhline(80, color='yellow', linestyle='-', linewidth=2, label='Quente (Atenção)')
+
+    plt.axhline(70, color='green', linestyle='-', linewidth=2, label='Frio (Normal)')
     plt.axhline(100, color='red', linestyle='-', linewidth=2, label='Muito Quente (Crítico)')
-    
 
-    if dados_y:
-        media = sum(dados_y) / len(dados_y)
-        plt.axhline(media, color='purple', linestyle='--', linewidth=2, label=f'Média: {media:.2f} °C')
+    media = sum(dados_y) / len(dados_y)
 
+    plt.axhline(media, color='purple', linestyle='-', linewidth=2, label='Média')
     plt.legend()
-    plt.xticks(rotation=45)
-    plt.tight_layout()
+    # plt.xticks(rotation=45)
+    # plt.tight_layout()
     plt.savefig("grafico_temperatura.png")
     plt.show()
 
-if __name__ == "__main__":
+if __name__ == "main":
     gerar_grafico_temperatura()
