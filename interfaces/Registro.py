b21838b6cfcea45b99ba48693c795036c12439fa
F3gcastro <gcastro@f3capital.com.br>
2025-05-20 20:53:27 -0300

    Habilitando novamente o envio dos nome do sensor de cada dado para a Azure.

--- interfaces/Registro.py
+++ interfaces/Registro.py
@@ -4,26 +4,26 @@ from interfaces.EnumCenarios import EnumCenarios
 
 
 class Registro:
-    sensor: str
+    nome_sensor: str
     unidade: str
     valor: float
     instante: str
     cenario: EnumCenarios
 
-    def __init__(self, sensor: str, unidade: str, valor: float, cenario: EnumCenarios) -> None:
-        self.sensor = sensor
+    def __init__(self, nome_sensor: str, unidade: str, valor: float, instante: datetime, cenario: EnumCenarios) -> None:
+        self.nome_sensor = nome_sensor
         self.unidade = unidade
         self.valor = valor
-        self.instante = datetime.now().isoformat()
+        self.instante = instante.isoformat()
         self.cenario = cenario
 
     def __repr__(self) -> str:
-        return f"{self.sensor}: {self.valor} {self.unidade}"
+        return f"{self.nome_sensor}: {self.valor} {self.unidade}"
 
     def to_json(self) -> dict:
         return {
             "deviceID": "eda-watson",
-            # "sensor": self.sensor,
+            "sensor": self.nome_sensor,
             "valueType": self.unidade,
             "value": self.valor,
             "instant": self.instante,
