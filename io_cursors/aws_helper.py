fe7dcd8e253558dae278dc99fcb6254fe8f37dc0
F3icastro <icastro@f3capital.com.br>
2025-10-19 17:51:10 -0300

    [WIP] Ajuste AWS

--- io_cursors/aws_helper.py
+++ io_cursors/aws_helper.py
@@ -1,3 +1,4 @@
+import awscrt.mqtt
 from awsiot import mqtt_connection_builder
 from awscrt import mqtt
 import os
@@ -7,39 +8,55 @@ import requests
 
 class AwsHelper:
 
-    device = mqtt_connection_builder
+    device = awscrt.mqtt.Connection
     topic = None
-    BASE_PATH = "./io_cursors/aws_config_files"
+    base_path = "./io_cursors/aws_config_files"
+    ROOT_CA1_URL = "https://www.amazontrust.com/repository/AmazonRootCA1.pem"
 
     def __init__(self):
-        self.gerenciar_arquivo_root()
-
         self.topic = os.getenv('TOPIC')
-        self.device = mqtt_connection_builder.mtls_from_path(
-            endpoint=os.getenv("ENDPOINT"),
-            cert_filepath=f"{self.BASE_PATH}/iot-certificate.pem.crt",
-            pri_key_filepath=f"{self.BASE_PATH}/iot-private-key.pem.crt",
-            ca_filepath=f"{self.BASE_PATH}/AmazonRootCA1.pem",
-            client_id=os.getenv("CLIENT_ID"),
-            keep_alive_secs = 30
-        )
-        self.device.connect().result()
-        print("Conexão bem estabelecida!")
+        self.base_path = os.path.abspath(self.base_path)
 
-    def gerenciar_arquivo_root(self):
-        if not os.path.exists(self.BASE_PATH):
-            os.makedirs(self.BASE_PATH)
+        self._baixar_arquivo_root()
+        self.device = self._criar_device()
 
-        if not os.path.exists(f'{self.BASE_PATH}/AmazonRootCA1.pem'):
+    def _baixar_arquivo_root(self):
+        if not os.path.exists(self.base_path):
+            os.makedirs(self.base_path)
+
+        full_filepath = f'{self.base_path}/AmazonRootCA1.pem'
+        if not os.path.exists(full_filepath):
             try:
-                root_ca1_url = "https://www.amazontrust.com/repository/AmazonRootCA1.pem"
-                response = requests.get(root_ca1_url)
+                response = requests.get(self.ROOT_CA1_URL)
                 response.raise_for_status()
-                with open('archives/AmazonRootCA1.pem','wb') as file:
+                with open(full_filepath,'wb') as file:
                     file.write(response.content)
-                    print('Arquivo ROOT CA1 da Amazon baixado com sucesso!')
+                    print('\tArquivo ROOT CA1 da Amazon baixado com sucesso!')
             except requests.exceptions.RequestException as e:
-                print(f'Ocorreu um erro na chamada:{e}')
+                print(f'\tOcorreu um erro na chamada:{e}')
+
+    def _criar_device(self) -> awscrt.mqtt.Connection:
+        ca_filepath = os.path.abspath(f"{self.base_path}/AmazonRootCA1.pem")
+
+        iot_config_files_path = os.getenv('IOT_CONFIG_FILES_PATH', None)
+        if iot_config_files_path is None or not os.path.exists(iot_config_files_path):
+            raise EnvironmentError(
+                f"O parâmetro .env com o caminho para os arquivos de configuração do IoT não foi configurado ou está "
+                f"inválido: \"{iot_config_files_path}\""
+            )
+
+        cert_filepath = os.path.abspath(f"{iot_config_files_path}/iot-certificate.pem.crt")
+        pri_key_filepath = os.path.abspath(f"{iot_config_files_path}/iot-private-key.pem.crt")
+        if not os.path.exists(cert_filepath) or not os.path.exists(pri_key_filepath):
+            raise FileNotFoundError(f"Ou o iot-certificate ou a iot-private-key não existem em \"{cert_filepath}\".")
+
+        device = mqtt_connection_builder.mtls_from_path(
+            cert_filepath=cert_filepath, pri_key_filepath=pri_key_filepath, ca_filepath=ca_filepath,
+            endpoint=os.getenv("ENDPOINT"), client_id=os.getenv("CLIENT_ID"), keep_alive_secs = 30
+        )
+        device.connect().result()
+        print("Conexão bem estabelecida!")
+        return device
         
     def send_data(self, dados: list[dict]):
         print("\n\tEnviando dados para Iot Core...")
