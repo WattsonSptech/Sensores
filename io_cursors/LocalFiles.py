cd02f5b6b2f73679ce407ec2ddee986c2daf32b5
edu-correa <eduardo.correa@sptech.school>
2025-11-04 22:14:18 -0300

    fix: fix name

--- io_cursors/LocalFiles.py
+++ io_cursors/LocalFiles.py
@@ -8,7 +8,7 @@ class LocalFiles:
     def salvar_local(dados: list[dict]):
         print("\n\tGravando dados para locamente...")
         path = "./logs"
-        filename = "telemetry-{}.json".format(datetime.now().strftime("%d-%m-%Y_%H-%M-%S .%f")[:-3])
+        filename = "telemetry_{}.json".format(datetime.now().strftime("%d-%m-%Y_%H-%M-%S .%f")[:-3])
 
         if not os.path.exists(path):
             os.mkdir(path)
